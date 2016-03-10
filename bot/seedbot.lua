c58802af03c657c1ee17c2e7ac60a7af1f2e35d7
shieldTM <hafez.janjan@gmail.com>
2016-03-09 20:55:50 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,29 +227,17 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {80182995,156823206,119650184},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
-An advanced administration bot based on TG-CLI written in Lua
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@Empero [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+    about_text = [[shield supergroups v1.0
+    ________________________
+    sudo
+    @hafez1116hafez
+    @amirho3ien911
+    @redcross_king
+    _______________________
+    کانال ما
+    @shieldTM
 ]],
     help_text_realm = [[
 Realm Commands:
